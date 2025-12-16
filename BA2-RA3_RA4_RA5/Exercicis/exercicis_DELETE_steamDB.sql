4c99290f8ccc145c10e4212f6e7f7fbbdeaf6cd8
victordomgs <victor.garcia.saiz@gmail.com>
2025-12-16 08:59:31 +0100

    Create exercicis_DELETE_steamDB.sql

---
+++ BA2-RA3_RA4_RA5/Exercicis/exercicis_DELETE_steamDB.sql
@@ -0,0 +1,28 @@
+/*******************************************************************************
+   Utilitzant la base de dades SteamDB, realitzeu els següents exercicis DELETE:
+********************************************************************************/
+
+-- **Exercici 1: Eliminar desenvolupadors**
+-- 1. Elimineu el desenvolupador japonès.
+-- 2. Esborreu l'estudi nord-americà de la taula "developers".
+-- 3. Suprimiu el desenvolupador polonès.
+
+-- **Exercici 2: Eliminar jocs**
+-- 1. Esborreu el joc d'aventura de la taula "games".
+-- 2. Elimineu el joc d'acció i món obert.
+-- 3. Suprimiu l'RPG desenvolupat per l'estudi polonès.
+
+-- **Exercici 3: Eliminar usuaris**
+-- 1. Esborreu l'usuari apassionat de "The Legend of Zelda".
+-- 2. Elimineu el fan de "Grand Theft Auto".
+-- 3. Suprimiu l'usuari fan de "The Witcher".
+
+-- **Exercici 4: Eliminar transaccions**
+-- 1. Esborreu la transacció del fan de "Grand Theft Auto".
+-- 2. Elimineu la transacció del joc "The Witcher 3".
+-- 3. Suprimiu la transacció del joc d'aventura.
+
+-- **Exercici 5: Eliminar ressenyes**
+-- 1. Esborreu la ressenya de "The Legend of Zelda".
+-- 2. Elimineu la ressenya de "Grand Theft Auto".
+-- 3. Suprimiu la ressenya de "The Witcher 3".
