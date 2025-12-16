06c6c1a666ae9fdc1066a55cbab3a368d5bf9588
victordomgs <victor.garcia.saiz@gmail.com>
2025-12-16 08:57:20 +0100

    Create exercicis_INSERT_steamDB.sql

---
+++ BA2-RA3_RA4_RA5/Exercicis/exercicis_INSERT_steamDB.sql
@@ -0,0 +1,35 @@
+/*******************************************************************************
+   Utilitzant la base de dades SteamDB inserta la següent informació: 
+********************************************************************************/
+
+-- Exercicis d'inserció SQL per als alumnes
+
+-- **Exercici 1: Afegir desenvolupadors**
+-- A la taula "developers", afegiu tres desenvolupadors amb les següents característiques:
+-- 1. Un desenvolupador japonès conegut pel seu treball en videojocs icònics com "Super Mario" i "The Legend of Zelda".
+-- 2. Un estudi nord-americà especialitzat en jocs d'acció i món obert com "Grand Theft Auto".
+-- 3. Un desenvolupador polonès famós per la sèrie "The Witcher".
+
+-- **Exercici 2: Afegir jocs**
+-- A la taula "games", afegiu tres jocs amb aquestes dades:
+-- 1. Un joc d'aventura llançat el 3 de març de 2017 per un desenvolupador japonès. El seu preu és de 59.99 euros i pertany al gènere d'aventures.
+-- 2. Un joc d'acció i món obert llançat el 17 de setembre de 2013 per un estudi nord-americà. Té un preu de 29.99 euros.
+-- 3. Un RPG premiat, llançat el 19 de maig de 2015, desenvolupat per un estudi polonès. El preu és de 39.99 euros.
+
+-- **Exercici 3: Afegir usuaris**
+-- A la taula "users", creeu tres usuaris amb la següent informació:
+-- 1. Un usuari apassionat de "The Legend of Zelda", amb el nom d'usuari que reflecteixi el seu amor per Link. Es va registrar el 1 de gener de 2024.
+-- 2. Un usuari entusiasta de "Grand Theft Auto", amb un correu electrònic relacionat amb el joc i un nom d'usuari adequat. Es va unir el 2 de gener de 2024.
+-- 3. Un fan incondicional de "The Witcher". El nom d'usuari ha de fer referència al títol del joc. Es va registrar el 3 de gener de 2024.
+
+-- **Exercici 4: Afegir transaccions**
+-- Registreu tres transaccions a la taula "transactions" amb les següents dades:
+-- 1. Una compra feta per l'usuari de "The Legend of Zelda" el 5 de gener de 2024 per un import de 59.99 euros.
+-- 2. Una transacció realitzada per l'usuari fan de "Grand Theft Auto" el 6 de gener de 2024, amb un import de 29.99 euros.
+-- 3. Una compra del joc "The Witcher 3" efectuada pel seu fan el 7 de gener de 2024, amb un cost de 39.99 euros.
+
+-- **Exercici 5: Afegir ressenyes**
+-- A la taula "reviews", afegiu les opinions dels usuaris sobre els seus jocs favorits:
+-- 1. Una ressenya de 5 estrelles per "The Legend of Zelda", destacant el seu món obert fascinant. La ressenya es va publicar el 10 de gener de 2024.
+-- 2. Una opinió de 4 estrelles sobre "Grand Theft Auto", mencionant la seva història captivadora. Publicada l'11 de gener de 2024.
+-- 3. Una ressenya de 5 estrelles per "The Witcher 3", descrivint-lo com el millor RPG de tots els temps. Escrita el 12 de gener de 2024.
